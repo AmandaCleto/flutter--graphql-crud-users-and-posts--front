bdd1edef533eca96af2ede2d7ec797182693e95f
Amanda Cleto <amanda.cleto.dev@gmail.com>
2023-01-19 16:47:42 -0300

    feat: create env file, initialize env and hive for flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:graphql_crud_users/views/index.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // initialize hive
+  await initHiveForFlutter();
+
+  await dotenv.load(fileName: ".env");
+
   runApp(const App());
 }
