aac8d872a6e0a90a67f80c60bc26d484fc3c2af2
AmandaCleto <amanda.cleto.dev@gmail.com>
2023-01-25 20:35:07 -0300

    fix: font sizes

--- lib/shared/theme/font_sizes.dart
+++ lib/shared/theme/font_sizes.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 @immutable
 class FontSizes {
-  static const extra = 28.0;
-  static const title = 22.0;
+  static const extra = 24.0;
+  static const title = 20.0;
   static const subtitle = 18.0;
   static const bodyText = 14.0;
   static const small = 12.0;
